7ea01b775733b7c43da121e6ff12011ff81e5fec
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-20 15:50:29 +0530

    added components

---
+++ src/component/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+      <div>
+          <h1>Welcome To Home Page</h1>
+      </div>
+  )
+}
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './component/Home';
+import Post from './component/Post';
+import Profile from './component/Profile';
 
-ReactDOM.render(,document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Home}></Route>
+      <Route path="/post" component={Post}></Route>
+      <Route path="/profile" component={Profile}></Route>
+    </div>
+  </BrowserRouter>
+  , document.getElementById('root'));
