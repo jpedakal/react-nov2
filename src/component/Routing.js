f6285842aecb702c833ea63d7a519edbcd4689eb
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-20 16:59:34 +0530

    Update Routing.js

--- src/component/Routing.js
+++ src/component/Routing.js
@@ -8,12 +8,12 @@ const Routing = () => {
     return (
         <BrowserRouter>
             <div>
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a class="navbar-brand" href="#">My Application</a>
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="/app">My Application</a>
                         </div>
-                        <ul class="nav navbar-nav">
+                        <ul className="nav navbar-nav">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/post">Post</Link></li>
                             <li><Link to="/profile">Profile</Link></li>
