df92d6188822a4c88867d2d5dc4dde119e532c17
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-20 17:29:30 +0530

    Update Profile.js

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Profile = () => {
-  return (
-      <div>
-          <h1>Welcome To Profile Page</h1>
-      </div>
-  )
+    return (
+        <div className="panel panel-success">
+            <div className="panel-heading">Profile</div>
+            <div className="panel-body">
+                <div className="jumbotron">
+                    <h2>Profile Page</h2>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Profile;
