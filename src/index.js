27fe9ef919320f150ca7652d590a31aa249543c7
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-20 16:54:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router-dom';
 import Routing from './component/Routing';
 
 ReactDOM.render(<Routing />, document.getElementById('root'));
