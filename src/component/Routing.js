ebb09d78c21da0ec9146d0006534f581fba2c10f
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-20 16:23:56 +0530

    update

--- src/component/Routing.js
+++ src/component/Routing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Post from './Post';
