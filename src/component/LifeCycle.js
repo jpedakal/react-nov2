6fb8c41261bc4f3022c4d6269fb622ca3edbc8ea
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-21 17:11:03 +0530

    Update LifeCycle.js

--- src/component/LifeCycle.js
+++ src/component/LifeCycle.js
@@ -10,15 +10,30 @@
 import React, {Component} from 'react';
 
 class LifeCycle extends Component{
+   // 1) Get Default State
     constructor(props){
         super(props)
 
+  // 2) Set Deafult State
         this.state={
             title: "React App"
         }
+        console.log("Inside constructor");
     }
-    render(){
 
+ //  3) Before get created
+      componentWillMount(){
+          console.log("Inside component will mount");
+      }
+
+  // 4) render
+    render(){
+        console.log("Inside render");
+       return(
+           <div>
+             <h1>{this.state.title}</h1>
+           </div>
+       )
     }
 }
 
