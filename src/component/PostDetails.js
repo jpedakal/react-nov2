2692c65e95de129f146e40279be995bca5ee1f4c
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-21 06:43:43 +0530

    Update PostDetails.js

---
+++ src/component/PostDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PostDetails = () => {
+    return (
+        <div className="panel panel-success">
+            <div className="panel-heading">Profile</div>
+            <div className="panel-body">
+                <div className="jumbotron">
+                    <h2>PostDetails Page</h2>
+                    <p>A post office is a public facility that provides mail services, including accepting of letters and parcels, providing post office boxes, and selling postage stamps, packaging, and stationery.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostDetails;
