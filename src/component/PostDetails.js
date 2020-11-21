2cbbdb54e320941fc85d2e68c581da454d9a34b6
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-21 09:19:43 +0530

    Update PostDetails.js

--- src/component/PostDetails.js
+++ src/component/PostDetails.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const PostDetails = (props) => {
-    console.log(props);
+    console.log(props.match.params.topic);
     return (
         <div className="panel panel-success">
             <div className="panel-heading">Profile</div>
             <div className="panel-body">
                 <div className="jumbotron">
-                    <h2>PostDetails Page</h2>
+                    <h2>PostDetails of {props.match.params.topic}</h2>
                     <p>A post office is a public facility that provides mail services, including accepting of letters and parcels, providing post office boxes, and selling postage stamps, packaging, and stationery.</p>
                 </div>
             </div>
