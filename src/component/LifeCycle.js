d929ba2035fc397c24279d872a7c5294a396bbaa
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-22 16:42:05 +0530

    Update LifeCycle.js

--- src/component/LifeCycle.js
+++ src/component/LifeCycle.js
@@ -15,7 +15,7 @@ class LifeCycle extends Component {
     constructor(props) {
         super(props)
 
-    // 2) Set Deafult State
+        // 2) Set Deafult State
         this.state = {
             title: "React App"
         }
@@ -27,13 +27,21 @@ class LifeCycle extends Component {
         console.log("Inside component will mount");
     }
 
+    componentWillUpdate() {
+        console.log("Inside component will update");
+    }
+
+    componentDidUpdate() {
+        console.log("Inside component Did update");
+    }
+
     // 4) render
     render() {
         console.log("Inside render");
         return (
             <div>
                 <h1>{this.state.title}</h1>
-                <div className="btn btn-success" onClick={()=>{this.setState({title: "This is My Application"})}}></div>
+                <div className="btn btn-success" onClick={() => { this.setState({ title: "This is My Application" }) }}></div>
             </div>
         )
     }
